99a161f7164374cfd9ec3ea60583c8c4ad55c89b
sahilpabale <dev.sahilpabale@gmail.com>
2021-10-25 22:46:48 +0530

    fixing cors

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(cors());
+app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api", router);
